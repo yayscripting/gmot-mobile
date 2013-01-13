ae7b79dd6a56a3e60025f2f2b6745d71e75495ed
Jesse Degger <jesse@yayscripting.nl>
2013-01-13 16:33:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <title>GMOT</title>
+    <link rel="stylesheet" href="style.css">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <link rel="apple-touch-icon" href="natives/img/icon_iphone.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="natives/img/icon_iphone4.png">
+    <link rel="apple-touch-startup-image" href="natives/img/startup_iphone.png">
+
+    <script src="mobile.js"></script>
+  </head>
+  <body>
+    <header>
+      <h1>GMOT - Game Maker Off Topic</h1>
+      <nav>
+        <a href=""><span>Don Sandros uitloggen</span></a>
+      </nav>
+    </header>
+    <section>
+      <h2>Algemeen</h2>
+      <nav>
+        <a href="" class="children"><div class="new"></div><span>Algemene Discussies (subboard?)</span><span class="child">Test</span></a>
+        <a href=""><div class="new"></div><span>Nieuws en Serieuze Discussies</span></a>
+        <a href=""><div></div><span>Websites</span></a>
+        <a href=""><div></div><span>Show &amp; Tell</span></a>
+        <a href=""><div class="new"></div><span>Stel jezelf voor</span></a>
+        <a href=""><div></div><span>Forumspellen &amp; Off-topic</span></a>
+      </nav>
+    </section>
+    <section>
+      <h2>Computers &amp; Elektronica</h2>
+      <nav>
+        <a href=""><div class="new"></div><span>Computers &amp; Internet</span></a>
+        <a href=""><div></div><span>Programmeren</span></a>
+        <a href=""><div class="new"></div><span>Games</span></a>
+      </nav>
+    </section>
+  </body>
+</html>
---
+++ mobile.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+	if (window.pageYOffset < 1) {
+		window.scrollTo(0, 1);
+	}
+}, false);
