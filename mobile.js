99d812a06395b5e8b7b66b95d61deb698584b356
Jesse Degger <jesse@yayscripting.nl>
2013-01-14 21:56:43 +0100

    Added iOS support (including onclick events)

--- mobile.js
+++ mobile.js
@@ -1,5 +1,38 @@
+var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+
 window.addEventListener('load', function () {
 	if (window.pageYOffset < 1) {
 		window.scrollTo(0, 1);
 	}
+	
+	if(iOS){
+	
+		var a=document.getElementsByTagName("a");
+		for(var i=0;i<a.length;i++)
+		{
+		
+		    a[i].onclick=function()
+		    {
+		    
+		    	var evalCode = this.getAttribute("data-onclick");
+		    	
+		    	if(evalCode){
+		    
+			    	var result = eval(evalCode);
+			    	
+			    	if(!result){
+			    	
+			    		return false;
+			    	
+			    	}			    	
+			    	
+			}
+		    
+		        window.location=this.getAttribute("href");
+		        return false
+		    }
+		}
+	
+	}
+	
 }, false);
